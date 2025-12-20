f93658f8d8436c050ea85a8ce0a6214132277199
JhonnyDL <jhonnydelgadillo003@gmail.com>
2025-12-20 19:37:50 -0400

    correccion

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className={`font-sans antialiased`}>
         {children}
         <Analytics />
